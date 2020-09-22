cb4602b3d76476a3505993d92b5c6b58db0596e2
Gurcharan Kaur <gurcharankaur@gurcharans-mbp.lan>
2020-09-22 17:14:17 -0400

    Updates

---
+++ precache-manifest.307de4fc05d37f51e0c9c42f7968f356.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f362d98bf3b182fb7b7ad0ce79ca4a",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "66f3cc7e399e7ca7ffcc",
+    "url": "/amazon-clone/static/css/main.c46108f6.chunk.css"
+  },
+  {
+    "revision": "4feb01c58ba7188d7ab1",
+    "url": "/amazon-clone/static/js/2.38971b66.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/amazon-clone/static/js/2.38971b66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66f3cc7e399e7ca7ffcc",
+    "url": "/amazon-clone/static/js/main.1aba985e.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  }
+]);
